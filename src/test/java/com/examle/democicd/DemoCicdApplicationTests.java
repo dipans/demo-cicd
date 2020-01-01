1a3cac9439d7150c77c8ac113b3822632a139bcd
Dipan Sutradhar <Dipan@NCFAST-Admins-MacBook-Pro.local>
2019-12-31 20:01:09 -0500

    Fixing test

--- src/test/java/com/examle/democicd/DemoCicdApplicationTests.java
+++ src/test/java/com/examle/democicd/DemoCicdApplicationTests.java
@@ -1,9 +1,13 @@
 package com.examle.democicd;
 
 import static org.assertj.core.api.Assertions.assertThat;
+
+import org.h2.security.auth.H2AuthConfig;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import com.examle.democicd.resource.PersonResource;
 
@@ -14,7 +18,9 @@ import com.examle.democicd.resource.PersonResource;
  * @author Dipan
  *
  */
-@SpringBootTest
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {DemoCicdApplication.class,
+		H2AuthConfig.class },  webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class DemoCicdApplicationTests {
 
 	@Autowired
